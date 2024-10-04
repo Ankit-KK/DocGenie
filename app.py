c62090b56549d7b19625734bb0e054f9f71ed629
Ankit Kumar <ankitashuk20@gmail.com>
2024-10-05 02:36:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from openai import OpenAI
 
 # Initialize OpenAI client
-api_key = st.secrets["openai_api_key"]  # Store your API key in Streamlit secrets
+api_key ="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"  # Store your API key in Streamlit secrets
 client = OpenAI(
     base_url="https://integrate.api.nvidia.com/v1",
     api_key=api_key
